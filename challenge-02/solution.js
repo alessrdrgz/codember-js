85ea1db2fc3810a86170ec717b04e07d7d194a55
alessrdrgz <alessrodrgz@gmail.com>
2022-11-16 08:34:47 +0100

    challenge 02

--- challenge-02/solution.js
+++ challenge-02/solution.js
@@ -1,23 +1,8 @@
 const encryptedMsg = '11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101'
-const min = 97 // ascii code of a
-const max = 122 // ascii code of z
+const ASCII_REGEX = /9[7-9]|1[0-1][0-9]|12[0-2]/g
 
-const phrase = []
+const msg = encryptedMsg.split(' ').map(encryptedWord => {
+  return encryptedWord.match(ASCII_REGEX).map(l => String.fromCharCode(l)).join('')
+}).join(' ')
 
-encryptedMsg.split(' ').forEach(encryptedWord => {
-  const letters = []
-  let currentLetter = ''
-  encryptedWord.split('').forEach(n => {
-    currentLetter += n
-    if (currentLetter >= min && currentLetter <= max) {
-      letters.push(currentLetter)
-      currentLetter = ''
-    }
-  })
-
-  let word = ''
-  for (const l of letters) word += String.fromCharCode(l)
-  phrase.push(word)
-})
-
-console.log(phrase.join(' '))
+console.log(`submit ${msg}`)
