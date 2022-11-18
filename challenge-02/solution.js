052c4d355a504183e8c76eda53ea5e4a11d63670
alessrdrgz <alessrodrgz@gmail.com>
2022-11-18 22:11:17 +0100

    shorter solution

--- challenge-02/solution.js
+++ challenge-02/solution.js
@@ -1,8 +1,6 @@
-const encryptedMsg = '11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101'
+const encryptedMsg = await fetch('https://codember.dev/encrypted.txt').then(r => r.text())
 const ASCII_REGEX = /9[7-9]|1[0-1][0-9]|12[0-2]/g
 
-const msg = encryptedMsg.split(' ').map(encryptedWord => {
-  return encryptedWord.match(ASCII_REGEX).map(l => String.fromCharCode(l)).join('')
-}).join(' ')
+const msg = encryptedMsg.replace(ASCII_REGEX, r => String.fromCharCode(r))
 
 console.log(`submit ${msg}`)
